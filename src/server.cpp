2ad693affacf7c209ab6a29a77713a1b0db02f99
Ayush Anand <36472216+ayushanand18@users.noreply.github.com>
2023-10-30 14:06:36 +0000

    update: adds server class

--- src/server.cpp
+++ src/server.cpp
@@ -43,3 +43,36 @@ public:
   // considering content as a text, will later generalise for all data types
   string content;
 };
+
+class Server{
+private:
+    vector<string> server_ips;
+    unordered_set<string> file_ids;
+    unordered_map<string, File> file_table;
+    unordered_map<string, string> file_hashes; // maps file_hash->file_id
+    // own ThreadPool of threads to submit tasks to
+    ThreadPool thread_exec;
+public:
+    // constructor to setup things
+    Server() {
+        // init all server_ips, file_ids, file_table
+        // attach signal handling to handleDownload and handleUpload function
+    }
+    // function to check user with user_id present or not
+    bool checkUserWithUserID(string user_id);
+    // function to signin with user_id, password
+    bool signInWithUserIDPassword(string user_id, string password);
+    // function to register with name, user_id, password
+    bool registerWithUserIDPassword(string name, string user_id, string password);
+    // function to replicate data across other servers, takes no argument and no return
+    // must register this as a periodic event (cron job)
+    void replicateDataAcrossServer();
+    // function to handleDownload
+    void handleDownload(FileTransit dataOfUser);
+    // function to handleUpload
+    void handleUpload(string name, string author, string permissions, unsigned int size, string content);
+    // function to check if file with the hash is already present on server or not
+    // in the file_hashes map
+    bool checkFilePresent(string file_hash);
+};
+
