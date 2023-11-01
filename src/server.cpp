cc1721adbef78ed7e41d68d5a63c39c6efa2cdbb
Amitkrsahu#472 <109278844+Amitkrsahu472@users.noreply.github.com>
2023-11-01 11:14:34 +0530

    update:add_split_func

--- src/server.cpp
+++ src/server.cpp
@@ -44,8 +44,19 @@ public:
   string content;
 };
 
-vector<string> split(string line, string delim) {
-  // "1 2 3" -> {"1", "2", "3"}
+vector<string> split(string s, string delimiter) {
+     size_t pos_start = 0, pos_end, delim_len = delimiter.length();
+    std::string token;
+    std::vector<std::string> res;
+
+    while ((pos_end = s.find(delimiter, pos_start)) != std::string::npos) {
+        token = s.substr (pos_start, pos_end - pos_start);
+        pos_start = pos_end + delim_len;
+        res.push_back (token);
+    }
+
+    res.push_back (s.substr (pos_start));
+    return res;
 }
 class Server{
 private:
